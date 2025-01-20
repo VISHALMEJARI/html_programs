21c260b6daa925791021d8392e6a6451f927bb21
vishalmejari <vishalmejari15125@gmail.com>
2025-01-20 16:14:44 +0530

    adding files

---
+++ myapp2/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+
+  return (
+    <div>
+      <marquee>
+        Welcome to vasantdada patil college of engineering
+      </marquee>
+      <h2>
+        This is jsx element
+      </h2>
+    </div>
+      
+  )
+}
+ 
+export default App
