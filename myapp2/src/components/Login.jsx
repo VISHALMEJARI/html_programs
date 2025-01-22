220fde4c498c598d3d0e92a629b984ebcbecc608
vishalmejari <vishalmejari15125@gmail.com>
2025-01-22 11:29:54 +0530

    adding files

--- myapp2/src/components/Login.jsx
+++ myapp2/src/components/Login.jsx
@@ -1,15 +1,44 @@
+import {useState} from 'react'
 function Login(){
+    const[username,setUsername]=useState("");
+    const[password, setPassword]=useState("");
+    const[errorMessage,setErrorMessage]=useState("");
+
+    const handleUserNameChange=(event)=>{
+        setUsername(event.target.value);
+    }
+    const handlePasswordChange=(event)=>{
+        setPassword(event.target.value);
+    }
+    const handleSubmit=(event)=>{
+        event.preventDefault();
+    
+    if(username===""||password===""){
+        setErrorMessage("please enter username & password")
+    }
+    if(username==='shivam'||password==='shivam'){
+        console.log("Login is success")
+    }
+    else{
+        setErrorMessage("Invalid username & password")
+    }
+    }
     return(
         <div>
-            <h2>This is Login.jsx component</h2>
-            <form>
-                <label htmlFor="username">UserName </label>
-                <input type="text" id="username"/><br/><br/>
-                <label htmlFor="pass">Password </label>
-                <input type="password" id="pass"/><br/><br/>
-                <button type="submit" id="submit">Submit</button>
+            <h1>Welcome  to Login Page</h1>
+            {errorMessage && <p className='error'>{errorMessage}</p>}
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="username">UserName </label>
+                    <input type="text" id="username" value={username} onChange={handleUserNameChange}/>
+                    <br/><br/>
+                    <label htmlFor="password">Password </label>
+                    <input type="password" id="password" value={password} onChange={handlePasswordChange}/>
+                    <br/><br/>
+                    </div>
+                <button type="submit">Login</button>
             </form>
         </div>
     )
 }
-export default Login;
+export default Login; 
